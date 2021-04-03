395fdbd545a78f4b4fcd161ee28352e549401c9d
feliciovcm <feliciovcm@gmail.com>
2021-04-03 19:29:36 -0300

    Config Stripes and add stripe API functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,9 +27,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
@@ -27,3 +37,27 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  //buscar as informações do preço do produto. Usar a pripria biblioteca api do stripe,
+  //passando o price api id, que pode ser adquirido no site da stripe, dentro do produto cadastrado.
+  const price = await stripe.prices.retrieve("price_1IcEwdHpdfK0unIquLg4SwAu", {
+    expand: ["product"],
+  });
+  //o expand product é utilizado para buscar todas as informações do produto, titulo, imagem, etc, possibilitando utilizar
+  //essas informações na aplicação.
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
